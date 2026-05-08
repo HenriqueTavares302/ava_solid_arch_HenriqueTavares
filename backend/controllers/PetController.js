7aeee12865ff2db87af70fcb991005cd0aa23d8d
henriquetavares <tavareshenrique302@gmail.com>
2026-05-08 19:00:38 -0300

    Listagem de Pets e Adoções do Usuário (getAllUserPets e getAllUserAdoptions)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -78,15 +78,35 @@ module.exports = class PetController {
         }
 
         static async getAllUserPets(req, res) {
-            res.status(200).json({ message: 'Em breve...'})
+            const token = getToken(req)
+            const user = await getUserByToken(token)
+
+            const pets = await Pet.find({ 'user._id' : user._id}).sort('-createdAt')
+
+            res.status(200).json({ 
+                success : true,
+                count : pets.length,
+                data : pets
+
+            })
         }
 
         static async getAllUserAdoptions(req, res) {
-            res.status(200).json({ message: 'Em breve...'})
+            const token = getToken(req)
+            const user = await getUserByToken(token)
+
+            const pets = await Pet.find({ 'adopter._id' : user._id }).sort('-createdAt')
+
+           res.status(200).json({ 
+                success: true,
+                count: pets.length,
+                data: pets
+           })
+            
         }
 
         static async getPetById(req, res) {
-            res.status(200).json({ message: 'Em breve...'})
+            res.status(200).json({ message: 'Em breve...' })
         }
 
         static async removePetById(req, res){
